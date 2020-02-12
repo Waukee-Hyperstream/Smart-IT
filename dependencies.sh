fa053004da2983c7aa61075ed4e6373a7ae0da49
Andrew Schwenk <32316640+adschw1@users.noreply.github.com>
2020-02-12 13:31:59 -0600

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -2,6 +2,9 @@
 
 echo Installing Dependencies for Smart-IT 2020.
 
+brew install portaudio
+brew link portaudio
+
 pip install SpeechRecognition==3.8.1
 pip install gTTS==2.1.1
 pip install PyAudio==0.2.11
