0e21a21db648f641f81f9e77117f08803c76f6b2
Andrew Schwenk <32316640+adschw1@users.noreply.github.com>
2020-02-26 09:39:24 -0600

    Make script work with a fresh pi

--- dependencies.sh
+++ dependencies.sh
@@ -2,9 +2,14 @@
 
 echo Installing Dependencies for Smart-IT 2020.
 
+sudo apt-get update 
+sudo apt-get upgrade 
+sudo apt-get dist-upgrade
+sudo apt-get install portaudio19-dev 
+sudo apt-get install python-pyaudio
+sudo pip install pyaudio
 pip install SpeechRecognition
 pip install GTTS
-sudo apt-get install python-pyaudio
 pip install playsound
 
 echo Completed Installing Dependencies.
